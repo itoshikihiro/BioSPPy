cbc1076a590aa5705d8e98aeae5153289791ac7a
Jie Lin <jlin246@emory.edu>
2018-09-12 00:45:23 -0400

    test.py is modified

--- test.py
+++ test.py
@@ -1,10 +1,26 @@
 from biosppy import storage
 from biosppy.signals import ecg
+from biosppy.signals import bvp
 
-# load raw ECG signal
-signal, mdata = storage.load_txt('./examples/ecg.txt')
+# THIS CODE IS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+# A TUTOR OR CODE WRITTEN BY OTHER STUDENTS - Jie Lin
+# Python 3
 
-# process it and plot
-out = ecg.ecg(signal=signal, sampling_rate=1000., show=True)
+#coding=utf-8
+"""
+@version: 1.0
+@author: Jie Lin
+@Mail: jlin246@emory.edu
+@file: test.py
+@time: 09/11/2018 12:39am
+"""
 
-# do something interesting to out
+#the main method
+if __name__ == '__main__':
+    # load raw ECG signal
+    signal, mdata = storage.load_txt('./examples/ecg.txt')
+
+    # process it and plot
+    out = ecg.ecg(signal=signal, sampling_rate=1000., show=True)
+
+    # do something interesting to out
